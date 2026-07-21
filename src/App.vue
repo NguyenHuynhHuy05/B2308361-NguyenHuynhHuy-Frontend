8ff9f96468d319270777d82a8799a25a3dbfd2dd
NguyenHuynhHuy05 <nguyenhuynhhuy10@gmail.com>
2026-07-21 20:02:11 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+
+export default{
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<template>
+  <div class="app">
+    <AppHeader />
+
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style>
+.page{
+  max-width: 400px;
+  margin: auto;
+}
+</style>
